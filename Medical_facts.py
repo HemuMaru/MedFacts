32a282812c39aa4d685a5caa1f70ac93c2749e9d
HemuMaru <149799383+HemuMaru@users.noreply.github.com>
2023-11-03 20:33:38 +0530

    Update Medical_facts.py

--- Medical_facts.py
+++ Medical_facts.py
@@ -9,7 +9,7 @@ import random
 background_image_html = f"""
 <style>
 body {{
-    background-image: url('https://github.com/HemuMaru/MedFacts/raw/main/party.jpg');
+    background-image: url('https://github.com/HemuMaru/MedFacts/blob/main/party.jpg');
     background-size: cover;
     background-repeat: no-repeat;
     background-attachment: fixed;
